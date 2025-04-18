c8424a42b4aa3eaca7e1c798074f78da01c9211f
Aldo Hernandez <aldo.flores.hrz@gmail.com>
2025-04-18 14:54:53 -0600

    basic menu split for nn and equations

--- interfaces/new.py
+++ interfaces/new.py
@@ -1,13 +1,37 @@
 import gradio as gr
 
-def greet(name, intensity):
-    return "Hello, " + name + "!" * int(intensity)
 
-demo = gr.Interface(
-    fn=greet,
-    inputs=["text", "slider"],
-    outputs=["text"],
-)
+def handle_dropdown(choice):
+    if choice == 1:
+        return gr.update(visible=True), gr.update(visible=False)
+    elif choice == 2:
+        return gr.update(visible=False), gr.update(visible=True)
+    else:
+        return gr.update(visible=False), gr.update(visible=False)
+
+with gr.Blocks() as demo:
+    
+    exp_name = gr.Textbox(label="Nombre del experimento")
+    exp_type = gr.Dropdown(
+        choices=["---","Ecuación Matemática", "Red Neuronal"],
+        value=None,
+        label="Tipo de experimento",
+        interactive=True,
+        type='index'
+    )
+    
+    with gr.Column(visible=False) as menuMath:
+        eval_func = gr.Textbox(label="Ecuación a evaluar")
+
+    
+    
+    with gr.Column(visible=False) as menuNN:
+        plc2 = gr.Textbox(value="Placeholder",interactive=False,label="Menu for Option 2")
+
+    exp_type.change(fn=handle_dropdown, 
+                    inputs=exp_type, 
+                    outputs=[menuMath, menuNN])
+
 
 if __name__ == "__main__":
     demo.launch()
